dd90fd80ddbb30beb652c7f2fbca36716169896a
amannnpatel <amanpatelj2@gmail.com>
2024-11-10 12:50:35 +0000

    Data Types

---
+++ basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"  //treat all code as  newer version
+
+//alert(3+3) //will give error bcz we r using nodejs
+
+console.log(3
+    +3); //readability should be high
+
+console.log("aman");
+
+let name ="Aman"
+let age = 18
+let isloggedin = true;
+let state;
+let city = null
+
+// some data types
+// number => 2 to power 53
+// bigint
+// string => "" (inside double quote)
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => used  for uniqueness
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof city);
+
+// typeof null is object 
+// typeof undefined is undefined means value is not defined
