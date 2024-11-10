a95acebb4595dff9fafa0968d2fe49e9735798f1
amannnpatel <amanpatelj2@gmail.com>
2024-11-10 07:02:49 +0000

    container debug done

---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountName = "Aman" //always use let to declare variable
+var accountCity = "ludhiana" //prefer not to use var to declare a variable
+accountState = "punjab" //without any declaration keyword we can declare a variable in js but its not good way
+
+accountName = "Patel"
+
+console.table([accountId,accountName,accountCity, accountState])
